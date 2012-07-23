34020f78126ac57ccc13c21aca58bf0e36fd65a7
Myles Horton <myles@getlantern.org>
2012-07-23 13:54:22 -0700

    Added dll to the jar with auto-extraction

--- src/main/java/org/lantern/win/WinProxy.java
+++ src/main/java/org/lantern/win/WinProxy.java
@@ -1,15 +1,29 @@
 package org.lantern.win;
 
+import java.io.Closeable;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+/**
+ * Class that wraps native calls to wininet.dll InternetSetOption and 
+ * InternetQueryOption functions to modifying proxy settings. This is 
+ * necessary to ensure other programs receive notifications of proxy setting
+ * changes, which direct modifications to the registry will not accomplish.
+ */
 public class WinProxy {
     
     public WinProxy() {
         final String name = "winproxy4j";
         final File lib = new File(name + ".dll");
         if (!lib.isFile()) {
-            throw new RuntimeException("Missing library at "+
-                lib.getAbsolutePath());
+            copyFromJar(lib);
+            if (!lib.isFile()) {
+                throw new RuntimeException("Missing library at "+
+                    lib.getAbsolutePath());
+            }
         }
         System.loadLibrary(name);
     }
@@ -19,4 +33,47 @@ public class WinProxy {
     public native boolean unproxy();
     
     public native String getProxy();
+    
+
+    private void copyFromJar(final File dest) {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = getClass().getClassLoader().getResourceAsStream(dest.getName());
+            os = new FileOutputStream(dest);
+            copy(is, os);
+        } catch (final IOException e) {
+        } finally {
+            closeQuietly(is);
+            closeQuietly(os);
+        }
+        
+    }
+    
+    private int copy(final InputStream is, final OutputStream os) 
+        throws IOException {
+        if (is == null) {
+            throw new NullPointerException("null input stream.");
+        }
+        if (os == null) {
+            throw new NullPointerException("null output stream.");
+        }
+        final byte[] buffer = new byte[4096];
+        int count = 0;
+        int n = 0;
+        while (-1 != (n = is.read(buffer))) {
+            os.write(buffer, 0, n);
+            count += n;
+        }
+        return count;
+    }
+
+    private void closeQuietly(final Closeable stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (final IOException e) {
+            }
+        }
+    }
 }
